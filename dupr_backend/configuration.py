ac420dd10f7d8c8533aa46f94d9cbcac804db32a
Todd Goodrich <toddagood@gmail.com>
2025-02-15 18:15:48 -0500

    u

--- dupr_backend/configuration.py
+++ dupr_backend/configuration.py
@@ -185,7 +185,7 @@ class Configuration:
     ) -> None:
         """Constructor
         """
-        self._base_path = "http://https://backend.mydupr.com" if host is None else host
+        self._base_path = "https://backend.mydupr.com" if host is None else host
         """Default Base url
         """
         self.server_index = 0 if server_index is None and host is None else server_index
@@ -504,7 +504,7 @@ class Configuration:
         """
         return [
             {
-                'url': "//https://backend.mydupr.com",
+                'url': "https://backend.mydupr.com",
                 'description': "No description provided",
             }
         ]
